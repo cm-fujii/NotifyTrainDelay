8e4600284e96605f994ae7a9e712fc12a5732502
cm-fujii <fujii.genki@classmethod.jp>
2019-05-02 16:46:47 +0900

    不要となったデプロイ用スクリプトを削除 #5

--- 02_package.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-BUCKET_NAME="cm-fujii.genki-deploy"
-
-sam package \
-    --output-template-file packaged.yaml \
-    --s3-bucket $BUCKET_NAME
--- 03_deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-WEBHOOK_URL="https://hoge"
-
-sam deploy \
-    --template-file packaged.yaml \
-    --stack-name NotifyTrainDelayToSlack \
-    --capabilities CAPABILITY_IAM \
-    --parameter-overrides SlackWebhookUrl=$WEBHOOK_URL
